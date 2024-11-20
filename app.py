f836c94c02371b5ed711e351f482a0d105a34973
Mr-Hemanth <140688314+Mr-Hemanth@users.noreply.github.com>
2024-11-20 20:05:24 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+class UploadForm(FlaskForm):
+    image = FileField('Image', validators=[DataRequired()])
+    comment = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Upload')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UploadForm()
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    
+    if form.validate_on_submit():
+        file = form.image.data
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # Here, you might want to save the comment in a database or a file
+        # For simplicity, we won't persist comments in this example
+        
+        return redirect(url_for('index'))
+    
+    return render_template('index.html', form=form, images=images)
+
+if __name__ == '__main__':
+    app.run(debug=True)

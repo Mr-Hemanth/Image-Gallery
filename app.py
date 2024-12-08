600240502bc6a6b747334ee9902ae4b7a6c35482
Mr-Hemanth <140688314+Mr-Hemanth@users.noreply.github.com>
2024-12-08 18:15:44 +0530

    Initial commit

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def index():
         return redirect(url_for('index'))
     
     return render_template('index.html', form=form, images=images)
-
 if __name__ == '__main__':
     app.run(debug=True)
     
